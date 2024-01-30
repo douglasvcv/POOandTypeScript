f7e1ac88b3719a52f5dedb0927056dca57bffae8
Douglas <douglasvitor1233@gmail.com>
2024-01-30 19:43:49 -0300

    add practice

--- Praticas/Pratica2.js
+++ Praticas/Pratica2.js
@@ -62,16 +62,41 @@ class Produto{
         this.Nome = Nome
         this.valor = valor
     }
-
+    
 }
-
-class Venda{
-    Vendedor = Vendedor
+class Venda extends Vendedor{
     ListaDeProduto = []
-    ValorTotal = 
-
-    AdicionaProduto(){
-        new Produto()
-        //continuar aqui https://dodev.cademi.com.br/area/produto/item/1106601
+    ValorTotal = 0
+    constructor(Nome, Salario){
+        super(Nome, Salario)
+    }
+    adicionarProduto(n){
+        this.ListaDeProduto.push(n)
+    }
+    calcularTotal(){
+        for(let i = 0; i < this.ListaDeProduto.length; i++){
+            
+            this.ValorTotal += this.ListaDeProduto[i].valor
+        
+        }
+        console.log(this.ValorTotal)
+    }
+    finalizarVenda(){
+        console.log(``)
     }
 }
+let venda1 = new Venda("Douglas", 3000)
+let produto1 = new Produto("bombril", 2)
+let produto2 = new Produto("jua", 7)
+
+
+venda1.adicionarProduto(produto1)
+venda1.adicionarProduto(produto2)
+
+console.log(venda1.ListaDeProduto)
+
+venda1.calcularTotal()
+
+
+
+console.log(venda1)
